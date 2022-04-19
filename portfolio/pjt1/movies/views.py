7637fc6fc0ee87aa283bd190f6341e0fabc936c9
beeee2 <beeee2@naver.com>
2022-04-19 23:16:20 +0900

    [수정] 메인화면 스킬셋 수정

--- portfolio/pjt1/movies/views.py
+++ portfolio/pjt1/movies/views.py
@@ -1,7 +1,13 @@
+from pprint import pprint
 from django.shortcuts import render
+import requests
+from pprint import pprint
+import random
 
 # Create your views here.
 def index(request):
+    card = list(range(8))
+    BASE_URL = ''
     return render(request, 'movies/index.html')
 
 def recommendations(request):
